8209be4719d234ac78303e1e0e9af0de8389f69f
michael <mike98989@gmail.com>
2024-09-04 22:06:48 +0200

    WIP

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
-
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': import('@typescript-eslint/eslint-plugin'),
+      'react': import('eslint-plugin-react'),
+    },
+    rules: {
+      'no-unused-vars': 'warn',   // Warn instead of error for unused variables
+      'semi': ['error', 'always'], // Ensure semicolons are always used
+      '@typescript-eslint/explicit-function-return-type': 'off', // Turn off explicit return type requirement
+      'react/prop-types': 'off',   // Turn off prop-types checks in React components
+    },
+  },
 ];
